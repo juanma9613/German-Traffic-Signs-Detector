be45d4b87b9c42cd6a4345baa736fa2b53e962a4
Juan Manuel <jmunozb@eafit.edu.co>
2018-05-14 21:39:14 -0500

    py adddownloadlink1

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import click
 
+import urllib.request
+import zipfile
+#url='http://www.mynikko.com/dummy/dummy12.zip'
+url='http://file-examples.com/wp-content/uploads/2017/02/zip_5MB.zip'
 
+# Download the file from `url` and save it locally under `file_name`:
+urllib.request.urlretrieve(url, 'file5mb.zip')
 @click.group()
 def main():
     pass
@@ -8,6 +14,10 @@ def main():
 @main.command("download")
 def download():
     print("here I should be downloading the data...")
+    url='http://file-examples.com/wp-content/uploads/2017/02/zip_5MB.zip'
+    urllib.request.urlretrieve(url, '/images/file5mb.zip')
+
+
     
 @main.command("train")
 @click.option('-m', '--model')
